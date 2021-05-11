4eb661b8a836a336aba3de18198d524e556c4ebc
Gleb Starostin <starostin.gm@yandex.ru>
2021-05-11 15:56:27 +0400

    API: added services DI registration

--- Orders.Api/Startup.cs
+++ Orders.Api/Startup.cs
@@ -1,12 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Orders.BusinessLogic;
+using Orders.Data;
 
 namespace Orders.Api
 {
@@ -16,6 +14,8 @@ namespace Orders.Api
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.RegisterOrdersData();
+            services.RegisterOrdersBuisnessLogic();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
