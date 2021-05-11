3053d4133a875b51d373b2aecfbd851e12634fbc
Gleb Starostin <starostin.gm@yandex.ru>
2021-05-11 16:10:46 +0400

    added controllers for orders and postamats

---
+++ Orders.Api/Controllers/OrdersController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Orders.BusinessLogic.Services.Orders.Interfaces;
+using Orders.BusinessLogic.Services.Orders.Models;
+
+namespace Orders.Api.Controllers
+{
+    [ApiController]
+    public class OrdersController : ControllerBase
+    {
+        private readonly IOrdersService _ordersService;
+
+        public OrdersController(
+            IOrdersService ordersService
+            )
+        {
+            _ordersService = ordersService;
+        }
+
+        [HttpGet("{orderId}")]
+        public IActionResult GetOrder(int orderId)
+        {
+            var order = _ordersService.GetOrder(orderId);
+            if (order == null)
+                return NotFound();
+
+            return Ok(order);
+        }
+
+        [HttpPut]
+        public void UpdateOrder(UpdateOrderRequest orderDto)
+        {
+            _ordersService.UpdateOrder(orderDto);
+        }
+
+        [HttpPost]
+        public void CreateOrder(CreateOrderRequest orderDto)
+        {
+            _ordersService.CreateOrder(orderDto);
+        }
+
+        [HttpDelete("{orderId}")]
+        public void CancelOrder(int orderId)
+        {
+            _ordersService.CancelOrder(orderId);
+        }
+    }
+}
---
+++ Orders.Api/Controllers/PostamatsController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Orders.BusinessLogic.Services.Postamats.Interfaces;
+
+namespace Orders.Api.Controllers
+{
+    [ApiController]
+    public class PostamatsController : ControllerBase
+    {
+        private readonly IPostamatsService _postamatsService;
+
+        public PostamatsController(
+            IPostamatsService postamatsService
+            )
+        {
+            _postamatsService = postamatsService;
+        }
+
+        [HttpGet("{postamatId}")]
+        public IActionResult GetOrder(string postamatId)
+        {
+            var postamat = _postamatsService.GetPostamat(postamatId);
+            if (postamat == null)
+                return NotFound();
+
+            return Ok(postamat);
+        }
+    }
+}
